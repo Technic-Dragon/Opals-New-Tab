3adba08e525b9ad5afe010f9f0397171785c6145
Technic-Dragon <87450243+Technic-Dragon@users.noreply.github.com>
2024-08-22 09:17:33 -0600

    Add files via upload

---
+++ linkConfig.js
@@ -0,0 +1,77 @@
+const linkConfig = [
+    {
+        category: "school/",
+        links: [
+            {
+                showAs: "schoology",
+                link: "https://adams12.schoology.com"
+            },
+            {
+                showAs: "classroom",
+                link: "https://classroom.google.com"
+            },
+            {
+                showAs: "college board",
+                link: "https://apstudents.collegeboard.org/"
+            },
+            {
+                showAs: "webassign",
+                link: "https://www.webassign.net/"
+            },
+            {
+                showAs: "turnitin",
+                link: "https://www.turnitin.com/s_home.asp?svr=6&lang=en_us&r=30.5809931943386"
+            },
+            {
+                showAs: "code.org",
+                link: "https://studio.code.org/home"
+            },
+            {
+                showAs: "infinite campus",
+                link: "https://ic.adams12.org/campus/adams12.jsp"
+            }
+        ]
+    },
+    {
+        category: "google suite/",
+        links: [
+            {
+                showAs: "school mail",
+                link: "https://mail.google.com/mail/u/0/#inbox"
+            },
+            {
+                showAs: "school drive",
+                link: "https://drive.google.com/drive/u/0/home"
+            },
+            {
+                showAs: "personal mail",
+                link: "https://mail.google.com/mail/u/1/#inbox"
+            },
+            {
+                showAs: "personal drive",
+                link: "https://drive.google.com/drive/u/1/home"
+            },
+            {
+                showAs: "work mail",
+                link: "https://mail.google.com/mail/u/2/#inbox"
+            },
+            {
+                showAs: "work drive",
+                link: "https://drive.google.com/drive/u/2/home"
+            }
+        ]
+    },
+    {
+        category: "other/",
+        links: [
+            {
+                showAs: "chat gpt",
+                link: "https://chatgpt.com/"
+            },
+            {
+                showAs: "github",
+                link: "https://github.com/"
+            }
+        ]
+    }
+]
